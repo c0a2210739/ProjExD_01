7d5b9041956d158048bb2cf6c5650ddf32b4c663
nagasawa <c0a2210739@edu.teu.ac.jp>
2023-09-26 15:35:13 +0900

    4回目コミット

--- pygame_image.py
+++ pygame_image.py
@@ -10,13 +10,17 @@ def main():
     ko_img = pg.image.load("ex01/fig/3.png")
     ko_img = pg.transform.flip(ko_img,True,False)
     ko2_img = pg.transform.rotozoom(ko_img,10,1.0)
-    lst = [ko_img,]
+    img_lst = [ko_img,ko2_img]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        if tmr % 2 == 1:
+            screen.blit(ko_img,[300,200])
+        else:
+            screen.blit(ko2_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
